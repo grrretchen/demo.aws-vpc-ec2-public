cd5ae44a6869f1f245514f4bf36ec48a9b2865f9
Gretchen Keppel <gretchen.keppel@gmail.com>
2024-04-14 07:38:40 -0500

    update files to follow terraform best practice

---
+++ terraform/variables-ops.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type    = string
+  default = "demo.aws-ec2-with-vpc"
+}
+
+variable "github-repo" {
+  type    = string
+  default = "demo.terraform-aws-vpc-ec2-public"
+}
+
