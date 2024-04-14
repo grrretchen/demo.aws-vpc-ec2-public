8ed9985de8428b3fa75218628be8f1b28c1fcc03
Gretchen Keppel <gretchen.keppel@gmail.com>
2024-04-13 20:32:48 -0500

    formatting

--- terraform/main-vpc.tf
+++ terraform/main-vpc.tf
@@ -1,4 +1,4 @@
-# Creating VPC
+# VPC
 resource "aws_vpc" "main" {
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
@@ -9,6 +9,8 @@ resource "aws_vpc" "main" {
   }
 }
 
+
+# VPC Subnet
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "${var.subnet1_cidr_public}"
@@ -20,18 +22,23 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_internet_gateway" "main" {
 
+# VPC Internet Gateway
+resource "aws_internet_gateway" "main" {
   tags = {
     Name = "my-internet-gateway"
   }
 }
 
+
+# VPC Internet Gateway Attachment
 resource "aws_internet_gateway_attachment" "main" {
   internet_gateway_id = aws_internet_gateway.main.id
   vpc_id              = aws_vpc.main.id
 }
 
+
+# VPC Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -40,12 +47,16 @@ resource "aws_route_table" "public" {
   }
 }
 
+
+# VPC Route Table Route
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
 
+
+# VPC Route Table Association
 resource "aws_route_table_association" "public_sn" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
--- terraform/variables-vpc.tf
+++ terraform/variables-vpc.tf
@@ -1,10 +1,10 @@
-# Defining CIDR Block for VPC
+# CIDR Block for VPC
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
 
-# Defining CIDR Block for 1st Subnet
+# CIDR Block for Public Subnet
 variable "subnet1_cidr_public" {
   type = string
   default = "10.0.0.0/24"
