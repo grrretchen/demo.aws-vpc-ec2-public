75f311e64ba6e1877a02af21cb896082b4f2a00f
Gretchen Keppel <gretchen.keppel@gmail.com>
2024-04-14 07:37:35 -0500

    rename files to follow terraform best-practice

--- terraform/main-compute.tf
+++ terraform/main-compute.tf
@@ -8,26 +8,14 @@ resource "aws_instance" "spec_webserver" {
 
   associate_public_ip_address = true
 
+  user_data_base64            = base64encode(data.local_file.userdata.content)
+  user_data_replace_on_change = false
+
   tags = {
     Name    = "Webserver"
     Project = var.project_name
   }
 
-  user_data_base64 = base64encode(data.local_file.userdata.content)
-#   user_data                   = data.local_file.userdata.content
-  user_data_replace_on_change = true
-  #   EOF
-
-  #   user_data = <<-EOF
-  #       #!/bin/bash
-  #       # Use this for your user data (script from top to bottom)
-  #       # install httpd (Linux 2 version)
-  #       yum update -y
-  #       yum install -y httpd
-  #       systemctl start httpd
-  #       systemctl enable httpd
-  #       echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-  #     EOF
 }
 
 # EC2 User Data
